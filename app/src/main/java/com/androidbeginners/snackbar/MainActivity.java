c9e96299fcf140d451f229e6432642c2d435959e
jobinj <jobinj1048@gmail.com>
2019-12-26 16:16:29 +0530

    simple snackbar

--- app/src/main/java/com/androidbeginners/snackbar/MainActivity.java
+++ app/src/main/java/com/androidbeginners/snackbar/MainActivity.java
@@ -1,14 +1,21 @@
 package com.androidbeginners.snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        parent = findViewById(R.id.parent);
+        Snackbar snackbar = Snackbar.make(parent, "Snackbar text",Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 }
